bea80f3779ffa91c3bfdecb745a08d169a30f147
Your Name <you@example.com>
2017-08-08 11:07:16 -0400

    initial commit, in-class practice

---
+++ Jackpot/Scripts/Script.js
@@ -0,0 +1,19 @@
+﻿function getRando(number) {
+    var num = Math.round(Math.random() * number); //generate number between 0 and 65
+
+    num = num.toString(); //turn into string so we can declare length
+
+    if (num.length < 2) { //add a second character
+        num = "0" + num;
+    }
+    return num;
+}
+
+function getPBNums() {
+    document.getElementById("PB1").innerHTML = getRando(65);
+    document.getElementById("PB2").innerHTML = getRando(65);
+    document.getElementById("PB3").innerHTML = getRando(65);
+    document.getElementById("PB4").innerHTML = getRando(65);
+    document.getElementById("PB5").innerHTML = getRando(65);
+    document.getElementById("PB6").innerHTML = getRando(65);
+}
